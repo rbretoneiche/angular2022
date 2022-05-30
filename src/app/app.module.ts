1b86a0930ef0308b65cf7d5750d5f53b72ae72e9
rbretoneiche <romainbretoneiche@gmail.com>
2022-05-30 10:27:02 +0200

    init 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import { CardComponent } from './components/card/card.component';
+import { CardComponent } from './card/card.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({

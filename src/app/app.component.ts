798954dae2b00496f4f6ab4ee78370c582f65350
rbretoneiche <romainbretoneiche@gmail.com>
2022-05-30 11:15:17 +0200

    init 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,20 @@ import {PokemonInterface} from "./interfaces/pokemon.interface";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  pikachu: PokemonInterface = {
-    name: 'Pikachu',
-    height: 40,
-    image: 'assets/pikachu.png',
-    weight: 600,
-    type: 'Electric'
-  }
-  bulbasaur: PokemonInterface = {
-    name: 'Bulbasaur',
-    height: 70,
-    image: 'assets/bulbi.svg',
-    weight: 690,
-    type: 'Grass'
-  }
+  pokemons: PokemonInterface[] = [
+    {
+      name: 'Pikachu',
+      height: 40,
+      image: 'assets/pikachu.png',
+      weight: 600,
+      type: 'Electric'
+    },
+    {
+      name: 'Bulbasaur',
+      height: 70,
+      image: 'assets/bulbi.svg',
+      weight: 690,
+      type: 'Grass'
+    }
+  ]
 }

90fdc69ce2175a427aa6a3e4ded0727eeedf0b60
rbretoneiche <romainbretoneiche@gmail.com>
2022-05-30 16:11:54 +0200

    init 10

--- src/app/modules/pokemon/pokemon.module.ts
+++ src/app/modules/pokemon/pokemon.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CardComponent} from "./components/card/card.component";
 import {HoverBackgroundDirective} from "./directives/hover-background.directive";
-import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
-import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import {PokemonListComponent} from './components/pokemon-list/pokemon-list.component';
+import {PokemonDetailComponent} from './components/pokemon-detail/pokemon-detail.component';
 import {RouterModule} from "@angular/router";
 
 
 @NgModule({
-  declarations: [CardComponent,
+  declarations: [
+    CardComponent,
     HoverBackgroundDirective,
     PokemonListComponent,
     PokemonDetailComponent,

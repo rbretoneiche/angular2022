9887fd8a218cc12993d0435dce7fb4b757d26048
romainbretoneiche <romainbretoneiche@gmail.com>
2022-06-15 19:42:50 +0200

    init formation 16.1

--- src/app/modules/pokemon/components/card/card.component.ts
+++ src/app/modules/pokemon/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {PokemonInterface} from "../../interfaces/pokemon.interface";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-card',
@@ -9,9 +10,20 @@ import {PokemonInterface} from "../../interfaces/pokemon.interface";
 export class CardComponent {
   @Input() pokemon?: PokemonInterface
   @Input() selectedPokemon?: PokemonInterface
+  @Input() isMine = false;
   @Output() selectedPokemonChanges = new EventEmitter<PokemonInterface>();
+  @Output() catchPokemonEvent = new EventEmitter<PokemonInterface>();
+
+  constructor(private router: Router) {
+  }
 
   handleClick() {
+    this.router.navigateByUrl(`/pokemon/${this.pokemon?.name}`)
     this.selectedPokemonChanges.emit(this.pokemon)
+  }
+
+  catch(event: Event) {
+    event.stopPropagation();
+    this.catchPokemonEvent.emit(this.pokemon)
   }
 }
--- src/app/modules/pokemon/components/pokemon-list/pokemon-list.component.ts
+++ src/app/modules/pokemon/components/pokemon-list/pokemon-list.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {PokemonService} from "../../services/pokemon.service";
 import {PokemonInterface} from "../../interfaces/pokemon.interface";
-import {Observable} from "rxjs";
+import {forkJoin, map, Observable} from "rxjs";
 import {Router} from "@angular/router";
 import {AuthService} from "../../../login/services/auth.service";
 import {AlertService} from "../../../../services/alert.service";
 import {AlertEnum} from "../../../../enums/alert.enum";
+import {UserHasPokemonInterface} from "../../interfaces/user-has-pokemon.interface";
 
 @Component({
   selector: 'app-pokemon-list',
@@ -17,16 +18,28 @@ export class PokemonListComponent implements OnInit {
   }
 
   pokemons$?: Observable<PokemonInterface[]>;
+  myPokemons$?: Observable<string[]>;
   selectedPokemon?: PokemonInterface;
   trackByIdentity = (index: number, item: PokemonInterface) => item.id;
 
   ngOnInit(): void {
     this.pokemons$ = this.pokemonService.getPokemon();
+    this.refreshMyPokemons()
   }
 
   handlePokemonChange(pokemon: PokemonInterface) {
     this.pokemonService.selectPokemon(pokemon);
     this.selectedPokemon = this.pokemonService.getSelectedPokemon()
+  }
+
+  handleCatchPokemon(pokemon: PokemonInterface) {
+    this.pokemonService.ownPokemon(pokemon).subscribe(() => {
+      this.refreshMyPokemons()
+    })
+  }
+
+  refreshMyPokemons(): void {
+    this.myPokemons$ = this.pokemonService.getMyPokemons().pipe(map(pokemons => pokemons.map(pokemon => pokemon.pokemonId)));
   }
 
   logout(): void {

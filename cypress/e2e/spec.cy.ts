faebbd2e36941221c13a072be8407f8b157254f3
romainbretoneiche <romainbretoneiche@gmail.com>
2022-06-16 12:56:24 +0200

    init formation 18

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe('Test pokemon app homepage', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/loign')
+    cy.visit('/login')
     cy.get('[data-cy=login-email-input]').type('sacha1@pokemon-form.fr')
     cy.get('[data-cy=login-password-input]').type('123456789')
     cy.get('[data-cy=login-submit-btn]').click().then(() => {
@@ -9,7 +9,7 @@ describe('Test pokemon app homepage', () => {
       cy.get('[data-cy=pokemon-list]')
         .should('exist')
         .find('app-card')
-        .should('have.length', 18)
+        .should('have.length', 21)
         .each((element) => {
           cy.wrap(element)
             .contains('Pokemon type')
@@ -18,6 +18,10 @@ describe('Test pokemon app homepage', () => {
           cy.wrap(element)
             .contains('Height')
         })
+      cy.get('[data-cy=logout-btn]')
+        .should('exist')
+        .click().then(() =>
+        cy.url().should('include', '/login'))
     })
   })
 })
